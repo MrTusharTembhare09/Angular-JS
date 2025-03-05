f93866cd3a8f7887f64e3f4eed9a8bf4c1c62d00
MrTusharTembhare09 <tushartembhare147@gmail.com>
2025-03-05 15:42:58 +0530

    [Tushar] Add. Implemented BridgeLabz logo display using Property Binding

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  message: string = 'Hello from BridgeLabz'; // Defining property for one-way binding
+  title = 'Helloworld';
+  
+  // Defining the BridgeLabz Logo URL
+  bridgeLabzLogo: string = 'https://images.yourstory.com/cs/images/companies/logo-1586419574337.jpg?fm=auto&ar=1%3A1&mode=fill&fill=solid&fill-color=fff&format=auto&w=384&q=75';
 }
 
